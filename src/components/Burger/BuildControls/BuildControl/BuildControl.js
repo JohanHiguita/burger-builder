6285cad6198b65f915052a9a62af0e5e3a2cebfc
JohanHiguita <higuitajohan@gmail.com>
2020-10-03 00:05:46 -0500

    Removing ingredients Safely

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,10 +5,14 @@ const buildControl = (props) => {
   return (
     <div className={classes.BuildControl}>
       <div className={classes.Label}>{props.label}</div>
-      <button className={classes.Less}>Less</button>
       <button 
-        className={classes.More}
-        onClick = {props.added}
+        className = {classes.Less}
+        onClick   = {props.removed}
+        disabled  = {props.disabled}
+        >Less</button>
+      <button 
+        className = {classes.More}
+        onClick   = {props.added}
         >
         More
       </button>
